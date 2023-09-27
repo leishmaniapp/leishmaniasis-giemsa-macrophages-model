e40492faa64da6836bf8a1395be6909d12f29e15
Nicolás Pérez Fonseca <n_perez@javeriana.edu.co>
2023-09-27 16:38:27 -0500

    False Macrophages Identification were fixed

--- MacrophagesIdentification.py
+++ MacrophagesIdentification.py
@@ -19,14 +19,42 @@ def coreIdentification(img):
         area = cv2.contourArea(cnt)
         if area < minArea:
             cv2.drawContours(mask,contours,i,color=(0,0,0),thickness=cv2.FILLED)
+
+    #Checking if the contoures detected are actual cores
+    mask2 = np.zeros((1944,2592 , 1), dtype = np.uint8)
+    #just for testing the funtion and its parameters
+    testImage = cv2.imread('MacropMask.jpg',0)
+    cores = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, 1, 160, param1=50, param2=5, minRadius=110, maxRadius=120)
+    circlesTest = img.copy()
+    cores = np.uint16(np.around(cores))
+    for (x,y,r ) in cores[0,:]:
+        cv2.circle(circlesTest, (x,y), r,(0,0,255),3)
+        cv2.circle(circlesTest, (x,y), 2,(0,0,255),3)
+
+    cv2.imwrite('CircleTest.jpg',circlesTest)
+    #Getting rid of the not rounded contours
+    contours, hierarchy1 = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for (x,y,r ) in cores[0,:]:
+        for i in range(len(contours)):
+            cnt = contours[i]
+            if 1 ==  cv2.pointPolygonTest(cnt,(x,y),False):
+                cv2.drawContours(mask2,contours,i,color=(255,255,255),thickness=cv2.FILLED)
+    
+    #Update found cores
+    cores = cv2.HoughCircles(mask2, cv2.HOUGH_GRADIENT, 1, 160, param1=50, param2=5, minRadius=110, maxRadius=120)
+    circlesTest = img.copy()
+    cores = np.uint16(np.around(cores))
+
+    #Writing the image files
+    cv2.imwrite('MaskTest.jpg',mask2)
     cv2.imwrite('CoreMask.jpg',mask)
     cv2.imwrite('CoreColored.jpg',imgColor)
 
-    return mask, imgColor
+    return mask2, imgColor, cores
 
 #Cytoplasm Identification
 
-def cytoplasmIdentification (img, coreMask, coreImgColor):
+def cytoplasmIdentification (img, coreMask, coreImgColor, cores):
     myColorFinder = ColorFinder(False)
     # Custom Color
     #hsv(264, 23%, 88%)
@@ -59,9 +87,21 @@ def cytoplasmIdentification (img, coreMask, coreImgColor):
         area = cv2.contourArea(cnt)
         if area < minArea:
             cv2.drawContours(diff2,contours,i,color=(0,0,0),thickness=cv2.FILLED)
-    cv2.imwrite('MacropMask.jpg',diff2)
 
-    return diff2
+    #Blank mask
+    mask2 = np.zeros((1944,2592 , 1), dtype = np.uint8)
+    #Checking if the cytoplasm detected are atatch to a sutable core
+    contours, hierarchy = cv2.findContours(diff2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    #Getting rid of not false macrophages identified
+    for (x,y,r ) in cores[0,:]:
+        for i in range(len(contours)):
+            cnt = contours[i]
+            if 1 ==  cv2.pointPolygonTest(cnt,(x,y),False):
+                cv2.drawContours(mask2,contours,i,color=(255,255,255),thickness=cv2.FILLED)
+
+    cv2.imwrite('MacropMask.jpg',mask2)
+
+    return mask2
 
 #Processing Images Method 1
 def processImage(imgPre, img):
@@ -132,18 +172,18 @@ def SorensenDiceCoeff (labeled, mask):
     return coeff
 
 #Reading the Image from the local storage
-img = cv2.imread('L3_M2_C12.png',1)
+img = cv2.imread('L24_M1_C1.png',1)
 
 #Preprocess Image - Core Identification (Part 1)
-coreMask, coreImgColor = coreIdentification(img)
+coreMask, coreImgColor , cores = coreIdentification(img)
 
 #Preprocess Image - Cytoplasm Identification (Part2)
-mask = cytoplasmIdentification(img, coreMask, coreImgColor)
+mask = cytoplasmIdentification(img, coreMask, coreImgColor, cores)
 
 #Process Image
 processImage(mask,img)
 
 #Metrics to measure masks
 #meanSquereError(labeled=mask,mask=CoreMask)
-#jaccardIndex(labeled=mask,mask=CoreMask)
+#jaccardIndex(labeled=mask,mask=CoreMask)111
 #SorensenDiceCoeff(labeled=mask,mask=CoreMask)
